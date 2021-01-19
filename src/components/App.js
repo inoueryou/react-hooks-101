8c872821c37ef0a55f249e3dcfd7f6bb3e1971f6
inouekatou <ino0607soccer@gmail.com>
2021-01-19 17:55:39 +0900

    display all operatin Logs

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import EventFrom from './EventFrom'
 import AppContext from '../contexts/AppContext'
 import Events from './Events'
+import OperationLogs from './OperationLogs'
 import reducer from '../reduers'
 
 
@@ -18,6 +19,7 @@ const App = () => {
       <div className="container-fluid">
         <EventFrom />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   )
