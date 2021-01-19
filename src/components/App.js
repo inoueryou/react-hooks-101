1639f52bdc3e216c66faf211f268f08c56b358e6
inouekatou <ino0607soccer@gmail.com>
2021-01-19 18:41:19 +0900

    save state with localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import EventFrom from './EventFrom'
 import AppContext from '../contexts/AppContext'
@@ -6,13 +6,19 @@ import Events from './Events'
 import OperationLogs from './OperationLogs'
 import reducer from '../reduers'
 
+const App_KEY = 'appwithRedux'
 
 const App = () => {
-  const initialState = {
+  const appState = localStorage.getItem(App_KEY)
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem(App_KEY, JSON.stringify(state))
+  }, [state])
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
